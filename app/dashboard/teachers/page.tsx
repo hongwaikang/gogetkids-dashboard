0ce6762481eb71c460bf2d9cbdfe0cbd512d7a9e
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-01-16 00:13:22 +0800

    Fixed Header on Teachers Page

--- app/dashboard/teachers/page.tsx
+++ app/dashboard/teachers/page.tsx
@@ -23,10 +23,10 @@ export default async function Page({
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Parents</h1>
+        <h1 className={`${lusitana.className} text-2xl`}>Teachers</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search parents..." />
+        <Search placeholder="Search teachers..." />
         <CreateTeacher />
       </div>
       <Suspense key={query + currentPage} fallback={<TeachersTableSkeleton />}>
