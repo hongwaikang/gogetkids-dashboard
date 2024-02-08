b09d7071660d6b12d69eb5e1030ff7628b0b8057
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-09 05:56:26 +0800

    Removed more unnecessary files

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,6 @@
 'use client';
 
 import {
-  UserGroupIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
-import {
   Groups as GroupsIcon,
   Groups2 as Groups2Icon,
   Groups3 as Groups3Icon,
@@ -20,17 +16,6 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  /*
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  {
-    name: 'Customers',
-    href: '/dashboard/customers',
-    icon: UserGroupIcon,
-  }, */
   {
     name: 'Classes',
     href: '/dashboard/classes',
