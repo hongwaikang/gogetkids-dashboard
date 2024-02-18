5985563cf7958e3f3c5c922b13ac493ac0c8e0e9
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-19 04:43:11 +0800

    set token to not null

--- app/dashboard/parents/page.tsx
+++ app/dashboard/parents/page.tsx
@@ -26,7 +26,7 @@ export default async function Page({
   // Decode the token to get school_name
   let schoolName;
   try {
-    const decodedToken: JwtPayload = jwt.verify(token, process.env.TOKEN_SECRET!) as JwtPayload;
+    const decodedToken: JwtPayload = jwt.verify(token!, process.env.TOKEN_SECRET!) as JwtPayload;
     schoolName = decodedToken.school_name;
   } catch (error) {
     console.error('Error decoding token:', error);
