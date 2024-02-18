897145eebdceb3ba7fca0bb1a3565b77b3927dea
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-19 05:31:16 +0800

    Revert change for logout route

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -6,15 +6,15 @@ export async function GET() {
     // Delete session document from 'sessions' collection
     await Session.deleteOne({ sessionName: 'currentSession' });
 
-    // Clear token cookie
     const response = NextResponse.json({
       message: "Logout successful",
       success: true,
     });
+
+    // Clear token cookie
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
 
-    // Redirect to '/login'
-    return NextResponse.redirect("/login");
+    return response;
 
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
