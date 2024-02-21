ff252262e3979892946ace639ba0d5037e993f4c
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-21 10:34:28 +0800

    shifted delete session syntax down

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -3,9 +3,6 @@ import Session from "@/app/models/sessionModel";  // Import Session model
 
 export async function GET() {
   try {
-    // Delete session document from 'sessions' collection
-    await Session.deleteOne({ sessionName: 'currentSession' });
-
     const response = NextResponse.json({
       message: "Logout successful",
       success: true,
@@ -13,6 +10,9 @@ export async function GET() {
 
     // Clear token cookie
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+
+    // Delete session document from 'sessions' collection
+    await Session.deleteOne({ sessionName: 'currentSession' });
 
     return response;
 
