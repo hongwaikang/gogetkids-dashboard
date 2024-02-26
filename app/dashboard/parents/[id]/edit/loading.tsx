441ce90c8f319509b934133bb4a3b7086e91c000
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-27 05:49:47 +0800

    changed loading page UI

--- app/dashboard/parents/[id]/edit/loading.tsx
+++ app/dashboard/parents/[id]/edit/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-    return <div>Loading...</div>;
+    return (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+            <div className="text-white text-6xl animate-fade-in">Loading...</div>
+        </div>
+    );
 }
