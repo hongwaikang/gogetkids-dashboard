af59fc91228f57943b2fb01fc17ca075baa9f829
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-28 02:58:38 +0800

    test

--- app/dashboard/classes/create/page.tsx
+++ app/dashboard/classes/create/page.tsx
@@ -1,49 +1,21 @@
-import Form from '@/app/ui/classes/create-form';
-import Breadcrumbs from '@/app/ui/classes/breadcrumbs';
-import { fetchAllTeachersEmail, fetchSessionToken, fetchSchoolName } from '@/app/lib/data';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import Form from '@/app/ui/teachers/create-form';
+import Breadcrumbs from '@/app/ui/teachers/breadcrumbs';
 
 export default async function Page() {
-
-  // Fetch session token
-  const sessionName = 'currentSession2'; // Adjust session name according to your setup
-  const token = await fetchSessionToken(sessionName);
-  console.log('Session token:', token);
-
-  // Verify and decode the token
-  let decodedToken: JwtPayload | string; // Explicitly type decodedToken
-  try {
-    // Type assertion to assert that token is a non-null string
-    decodedToken = jwt.verify(token!, process.env.TOKEN_SECRET!) as JwtPayload;
-    console.log('Decoded token data:', decodedToken);
-  } catch (error) {
-    console.error('Error verifying token:', error);
-    // Handle error if token verification fails or token is null
-    return null; // Or handle the error in some other way
-  }
-
-  // Extract user ID from decoded token
-  const sessionUserId = typeof decodedToken === 'string' ? decodedToken : decodedToken?.id;
-
-  // Fetch the company name using the user ID
-  const schoolName = await fetchSchoolName(sessionUserId);
-  console.log('School Name:', schoolName);
-
-  const teachers = await fetchAllTeachersEmail(schoolName);
 
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Classes', href: '/dashboard/classes' },
+          { label: 'Teachers', href: '/dashboard/teachers' },
           {
-            label: 'Edit Class',
-            href: `/dashboard/classes/create/`,
+            label: 'Create Teacher',
+            href: '/dashboard/teachers/create',
             active: true,
           },
         ]}
       />
-      <Form />
+      <Form/>
     </main>
   );
 }
