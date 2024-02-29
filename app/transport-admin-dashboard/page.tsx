cc5449759e8af9c512db80c2c415e0d26a72d80b
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-03-01 02:14:45 +0800

    updated dashboard pages to display more user info

--- app/transport-admin-dashboard/page.tsx
+++ app/transport-admin-dashboard/page.tsx
@@ -5,13 +5,19 @@ import React, { useState, useEffect } from "react";
 
 export default function DashboardPage() {
 
-  const [email, setEmail] = useState("No email");
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [role, setRole] = useState("");
 
   const getUserDetails = async () => {
     try {
       const res = await axios.get('/api/dashboard');
       console.log(res.data);
       setEmail(res.data.data.email);
+      setFirstname(res.data.data.firstname);
+      setLastname(res.data.data.lastname);
+      setRole(res.data.data.role);
     } catch (error: any) {
       console.error(error.message);
       // Handle errors if necessary
@@ -23,7 +29,7 @@ export default function DashboardPage() {
     getUserDetails();
   }, []); // The empty dependency array ensures that it only runs once when the component is mounted
 
-	return (
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen py-6 bg-white">
       <h1 className="text-4xl font-bold mb-4 text-blue-600">Dashboard</h1>
       <div className="bg-white p-6 rounded-md shadow-md text-center">
@@ -31,11 +37,22 @@ export default function DashboardPage() {
         <hr className="my-4 border-t border-gray-300" />
         <div className="bg-blue-50 p-4 rounded-md">
           <h2 className="text-lg font-semibold mb-2 text-blue-600">
-            {email === 'No email' ? "No Email" : "Your Email:"}
+            {email === 'No email' ? "Not logged in" : "Your Profile:"}
           </h2>
-          <p className="text-gray-800">
-            {email}
-          </p>
+          <div className="flex flex-col items-center">
+            <p className="text-gray-800">
+              <span className="font-semibold">Email:</span> {email}
+            </p>
+            <p className="text-gray-800">
+              <span className="font-semibold">First Name:</span> {firstname}
+            </p>
+            <p className="text-gray-800">
+              <span className="font-semibold">Last Name:</span> {lastname}
+            </p>
+            <p className="text-gray-800">
+              <span className="font-semibold">Role:</span> {role}
+            </p>
+          </div>
         </div>
       </div>
     </div>
