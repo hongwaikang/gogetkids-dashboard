70860cc38f8bf894a183c7ecc1c861f67611090c
hongwaikang <78042321+hongwaikang@users.noreply.github.com>
2024-02-29 19:39:52 +0800

    push

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,25 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to GoGetKids.</strong> This is the dashboard for the admin users
+            <strong>Welcome to GoGetKids.</strong> Simplify school pick-ups and enhance safety.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <p className="text-gray-600">
+            Transform your school's operations with GoGetKids. Our platform is designed to keep students safe, parents informed, and administrators in control.
+          </p>
+          <ul className="list-disc list-inside text-gray-600">
+            <li>✔️ Faster Dismissals</li>
+            <li>✔️ Real-time Student Tracking</li>
+            <li>✔️ Personalized Schedules</li>
+            <li>✔️ Clear Communication</li>
+            <li>✔️ Secure Access for Parents and Teachers</li>
+            <li>✔️ Custom Alerts and Notifications</li>
+            <li>✔️ And More...</li>
+          </ul>
           <Link
             href="/signup"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Sign Up</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Sign Up Here</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
